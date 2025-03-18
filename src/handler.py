f2753047bce1a1a918b0510123549bc53c1e0f7b
Alex Bates <alexlbates@gmail.com>
2025-03-18 17:38:31 -0400

    flesh out more of the actual method calls that will be used (still stub data though)

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,14 @@
 import logging
 import os
 import urllib3
+import boto3
+import json
 
 logger = logging.getLogger(__name__)
 http = urllib3.PoolManager()
+s3 = boto3.resource('s3')
+sqs = boto3.client('sqs')
+
 
 # TODO - The do they have a shared schema they want to work with, other than "here's an example json?"
 # TODO - are we supposed to be handling the infrastructure piece as well? that's a whole other thing. (CF/TF/Pulumi/???, IAM standards, CW logging standards, etc.)
@@ -13,13 +18,13 @@ http = urllib3.PoolManager()
 def lambda_handler(event, context):
     source_bucket = os.environ.get('SOURCE_BUCKET')
     destination_bucket = os.environ.get('DESTINATION_BUCKET')
-    destination_sqs = os.environ.get('DESTINATION_SQS_URL')  # TODO -- not needed if using destinations directly
+    destination_sqs = os.environ.get('DESTINATION_SQS_URL')
     activity_service_url = os.environ.get('ACTIVITY_SERVICE_URL')
 
     try:
         if not source_bucket or not destination_bucket:
             raise Exception(f'Missing runtime configuration - source/destination buckets - [{str(source_bucket)} | {str(destination_bucket)}]')
-        if not destination_sqs: #TODO probably not needed
+        if not destination_sqs:
             raise Exception('Missing runtime configuration - destination SQS url')
         if not activity_service_url:
             raise Exception(f'Missing runtime configuration - activity_service url')
@@ -29,71 +34,53 @@ def lambda_handler(event, context):
             raise Exception('field "uuid" not provided in event')
 
         logger.info(f'beginning operation for uuid: {document_uuid}')
-        attachment_data = process_uuid_attachments(document_uuid, activity_service_url)
-        form_response(document_uuid, attachment_data)
+        attachment_data = process_uuid_attachments(document_uuid, activity_service_url, source_bucket, destination_bucket)
+        send_sqs_updates(document_uuid, attachment_data, destination_sqs)
 
     except Exception as e:
         logger.error(f'Operation Failed')
         raise e
 
-def process_uuid_attachments(uuid:str, activity_service_url: str) -> object:
+
+def process_uuid_attachments(uuid: str, activity_service_url: str, source_bucket: str, destination_bucket: str) -> object:
     resp = http.request(
         'GET',
-        f'activity_service_url/{uuid}',
+        f'{activity_service_url}/{uuid}',
     )
 
     if resp.status != 200:
         raise Exception(f'status {resp.status} from {activity_service_url} for uuid {uuid}')
 
     attachment_data = resp.json()
-    if not attachment_data.get('???'):
+
+    # TODO - not sure what i'm meant to actually be pulling from this API that would allow me to copy stuff
+    if not attachment_data.get('folder'):
         raise Exception(f'missing attachment data for uuid {uuid}')
 
-    # TODO - iterate through results and foreach perform copy?
-    #        this part needs clarification from andrew
-    #        in terms of concrete example responses and specifically how this api interacts with the actual attachments
+    copy_source = {
+        'Bucket': source_bucket,
+        'Key': 'mykey' #TODO
+    }
 
-    # for x in attachment_data:
-    #   copy_files_between_s3_buckets(x)
-    #                            import boto3
-    #                            s3 = boto3.resource('s3')
-    #                            copy_source = {
-    #                                'Bucket': 'amzn-s3-demo-bucket1',
-    #                                'Key': 'mykey'
-    #                            }
-    #                            s3.meta.client.copy(copy_source, 'amzn-s3-demo-bucket2', 'otherkey')
-    #
-    #
-    # return /maybe the attachment data object?/
+    s3.meta.client.copy(copy_source, destination_bucket, 'some_key_here') #TODO
 
-def form_response(document_uuid: str, attachment_data: object) -> dict:
-    # TODO - need to talk about expectations around how results are being communicated for the lambda
-    #        use of destinations vs boto sqs messages, plus working examples to test with/hard confirmation on message shape
+    return attachment_data #TODO : what data is actually needed to form a final sqs message later?
 
 
+def send_sqs_updates(document_uuid: str, attachment_data: object, destination_sqs: str) -> tuple[dict, dict]:
+    # TODO - need to verify basically everything about the expected output
+    file_upload_message = {} #TODO
+    folder_created_message = {} #TODO
 
-    #       sqs = boto3.client('sqs')
-    #       response = sqs.send_message(
-    #           QueueUrl=queue_url,
-    #           MessageAttributes={
-    #               'Title': {
-    #                   'DataType': 'String',
-    #                   'StringValue': 'The Whistler'
-    #               },
-    #               'Author': {
-    #                   'DataType': 'String',
-    #                   'StringValue': 'John Grisham'
-    #               },
-    #               'WeeksOn': {
-    #                   'DataType': 'Number',
-    #                   'StringValue': '6'
-    #               }
-    #           },
-    #           MessageBody=(
-    #               'Information about current NY Times fiction bestseller for '
-    #               'week of 12/11/2016.'
-    #           )
-    #       )
-    #
-    #       print(response['MessageId'])
-    return 42
+    sqs.send_message(
+        QueueUrl=destination_sqs,
+        MessageAttributes={},
+        MessageBody=json.dumps(folder_created_message)
+    )
+    sqs.send_message(
+        QueueUrl=destination_sqs,
+        MessageAttributes={},
+        MessageBody=json.dumps(file_upload_message)
+    )
+
+    return file_upload_message, folder_created_message
--- test/test_handler.py
+++ test/test_handler.py
@@ -1,38 +1,42 @@
 import os
 import unittest
+from unittest.mock import patch, Mock
 
-from src.handler import form_response, lambda_handler
+from src.handler import lambda_handler
 
 
 class TestHandler(unittest.TestCase):
-    def tearDown(self):
-        os.environ['SOURCE_BUCKET'] = ""
-        os.environ['DESTINATION_BUCKET'] = ""
-        os.environ['DESTINATION_SQS_URL'] = ""
-        os.environ['ACTIVITY_SERVICE_URL'] = ""
+
+    def setUp(self):
+        os.environ['SOURCE_BUCKET'] = "some_value"
+        os.environ['DESTINATION_BUCKET'] = "some_value"
+        os.environ['DESTINATION_SQS_URL'] = "some_value"
+        os.environ['ACTIVITY_SERVICE_URL'] = "some_value"
 
     def test_lambda_handler_missing_source_bucket_throws(self):
         os.environ['SOURCE_BUCKET'] = ""
 
-        with self.assertRaisesRegex(Exception,'.*buckets'):
+        with self.assertRaisesRegex(Exception, '.*buckets'):
             lambda_handler({}, None)
 
     def test_lambda_handler_missing_destination_sqs_throws(self):
-        os.environ['SOURCE_BUCKET'] = "src"
-        os.environ['DESTINATION_BUCKET'] = "dest"
         os.environ['DESTINATION_SQS_URL'] = ""
 
-        with self.assertRaisesRegex(Exception,'.*destination SQS url'):
+        with self.assertRaisesRegex(Exception, '.*destination SQS url'):
             lambda_handler({}, None)
 
     def test_lambda_handler_missing_activity_service_key(self):
-        os.environ['SOURCE_BUCKET'] = "src"
-        os.environ['DESTINATION_BUCKET'] = "dest"
-        os.environ['DESTINATION_SQS_URL'] = "sqs"
+        os.environ['ACTIVITY_SERVICE_URL'] = ""
 
-        with self.assertRaisesRegex(Exception,'.*activity_service url'):
+        with self.assertRaisesRegex(Exception, '.*activity_service url'):
             lambda_handler({}, None)
 
-    def test_whatever_this_function_is_called(self):
-        self.assertEqual(42, form_response())
+    @patch('src.handler.http')
+    def test_lambda_handler_failed_activity_service_call(self, mock_http):
+        mock_response = Mock()
+        mock_response.status = 200
+        mock_response.json = Mock(return_value={'status': 'OK'})
+        mock_http.request.return_value = mock_response
 
+        with self.assertRaisesRegex(Exception, ".*missing attachment.*"):
+            lambda_handler({'uuid': 'asdf'}, None)
